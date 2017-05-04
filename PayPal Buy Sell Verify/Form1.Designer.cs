39841f71a39b3e622539021a6846854260952d47
poweredscript <poweredscript@gmail.com>
2017-05-04 07:13:49 +0700

    Add files via upload

---
+++ PayPal Buy Sell Verify/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace PayPal_Buy_Sell_Verify
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonCheck = new System.Windows.Forms.Button();
+            this.labelID = new System.Windows.Forms.Label();
+            this.labelReport = new System.Windows.Forms.Label();
+            this.buttonFacebookMessageCheck = new System.Windows.Forms.Button();
+            this.labelVerifyListUpdate = new System.Windows.Forms.Label();
+            this.verifyListOpen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(137, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(421, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // buttonCheck
+            // 
+            this.buttonCheck.Location = new System.Drawing.Point(458, 42);
+            this.buttonCheck.Name = "buttonCheck";
+            this.buttonCheck.Size = new System.Drawing.Size(100, 23);
+            this.buttonCheck.TabIndex = 1;
+            this.buttonCheck.Text = "ตรวจสอบ";
+            this.buttonCheck.UseVisualStyleBackColor = true;
+            this.buttonCheck.Click += new System.EventHandler(this.buttonCheck_Click);
+            // 
+            // labelID
+            // 
+            this.labelID.AutoSize = true;
+            this.labelID.Location = new System.Drawing.Point(3, 19);
+            this.labelID.Name = "labelID";
+            this.labelID.Size = new System.Drawing.Size(128, 13);
+            this.labelID.TabIndex = 2;
+            this.labelID.Text = "ID/Facebook Profile URL";
+            // 
+            // labelReport
+            // 
+            this.labelReport.AutoSize = true;
+            this.labelReport.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelReport.Location = new System.Drawing.Point(2, 68);
+            this.labelReport.Name = "labelReport";
+            this.labelReport.Size = new System.Drawing.Size(241, 24);
+            this.labelReport.TabIndex = 3;
+            this.labelReport.Text = "ใส่ข้อมูล จากนั้นคลิกปุ่มตรวจสอบ";
+            // 
+            // buttonFacebookMessageCheck
+            // 
+            this.buttonFacebookMessageCheck.Location = new System.Drawing.Point(403, 102);
+            this.buttonFacebookMessageCheck.Name = "buttonFacebookMessageCheck";
+            this.buttonFacebookMessageCheck.Size = new System.Drawing.Size(155, 23);
+            this.buttonFacebookMessageCheck.TabIndex = 4;
+            this.buttonFacebookMessageCheck.Text = "เปิดหน้า facebook messages";
+            this.buttonFacebookMessageCheck.UseVisualStyleBackColor = true;
+            this.buttonFacebookMessageCheck.Click += new System.EventHandler(this.buttonFacebookMessageCheck_Click);
+            // 
+            // labelVerifyListUpdate
+            // 
+            this.labelVerifyListUpdate.AutoSize = true;
+            this.labelVerifyListUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.labelVerifyListUpdate.ForeColor = System.Drawing.Color.Red;
+            this.labelVerifyListUpdate.Location = new System.Drawing.Point(134, 47);
+            this.labelVerifyListUpdate.Name = "labelVerifyListUpdate";
+            this.labelVerifyListUpdate.Size = new System.Drawing.Size(176, 13);
+            this.labelVerifyListUpdate.TabIndex = 6;
+            this.labelVerifyListUpdate.Text = "กำลังโหลดรายชื่อ โปรดรอสักครู่";
+            // 
+            // verifyListOpen
+            // 
+            this.verifyListOpen.Location = new System.Drawing.Point(6, 102);
+            this.verifyListOpen.Name = "verifyListOpen";
+            this.verifyListOpen.Size = new System.Drawing.Size(151, 23);
+            this.verifyListOpen.TabIndex = 7;
+            this.verifyListOpen.Text = "ดูรายชื่อผู้ผ่านการ verify แล้ว";
+            this.verifyListOpen.UseVisualStyleBackColor = true;
+            this.verifyListOpen.Click += new System.EventHandler(this.verifyListOpen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 132);
+            this.Controls.Add(this.verifyListOpen);
+            this.Controls.Add(this.labelVerifyListUpdate);
+            this.Controls.Add(this.buttonFacebookMessageCheck);
+            this.Controls.Add(this.labelReport);
+            this.Controls.Add(this.labelID);
+            this.Controls.Add(this.buttonCheck);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "PayPal Buy Sell Verify";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonCheck;
+        private System.Windows.Forms.Label labelID;
+        private System.Windows.Forms.Label labelReport;
+        private System.Windows.Forms.Button buttonFacebookMessageCheck;
+        private System.Windows.Forms.Label labelVerifyListUpdate;
+        private System.Windows.Forms.Button verifyListOpen;
+    }
+}
+
---
+++ PayPal Buy Sell Verify/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace PayPal_Buy_Sell_Verify
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            buttonFacebookMessageCheck.Enabled = false;
+            
+        }
+        List<string> _verifyList = new List<string>();
+        [STAThread]
+        private void buttonCheck_Click(object sender, EventArgs e)
+        {
+            buttonCheck.Enabled = false;
+            buttonFacebookMessageCheck.Enabled = false;
+           
+            //labelReport.Text = @"";
+            labelReport.Invoke(new MethodInvoker(delegate
+            {
+                labelReport.ForeColor = Color.Black;
+                labelReport.Text = @"กำลังตรวจสอบ โปรดรอสักครู่.";
+            }));
+
+
+            Uri uriResult;
+            bool isUrl = Uri.TryCreate(textBox1.Text, UriKind.Absolute, out uriResult) &&
+                         (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+            string upperId;
+            if (isUrl)
+            {
+                upperId = textBox1.Text;
+                buttonCheck.Enabled = true;
+            }
+            else
+            {
+                upperId = textBox1.Text.ToUpper();
+
+                if (!upperId.StartsWith("PP"))
+                {
+                    upperId = "PP" + upperId;
+                }
+                if (!upperId.EndsWith("A"))
+                {
+                    upperId = upperId + "A";
+                }
+                if (upperId.Length != 7)
+                {
+                    labelReport.ForeColor = Color.Orange;
+                    labelReport.Text = @"โปรดกรอกข้อมูล ช่อง ID ให้ถูกต้อง เช่น 0123 หรือ PP0123A";
+                    buttonCheck.Enabled = true;
+                    return;
+                }
+            }
+            try
+            {
+                labelReport.Text = @"กำลังตรวจสอบ โปรดรอสักครู่..";
+                if (_verifyList.Count <= 0)
+                {
+                    GetVerifyList();
+                }
+            }
+            catch (Exception ex)
+            {
+                labelReport.ForeColor = Color.Orange;
+                labelReport.Text = ex.Message;
+                buttonCheck.Enabled = true;
+                buttonCheck.Enabled = true;
+                return;
+            }
+            labelReport.Text = @"กำลังตรวจสอบ โปรดรอสักครู่...";
+            if (_verifyList.Count < 100)
+            {
+                labelReport.ForeColor = Color.Orange;
+                labelReport.Text = @"ไม่สามารถดาวโหลดรายชื่อ โปรดตรวจสอบการเชื่อมต่ออินเตอร์เน็ต";
+                buttonCheck.Enabled = true;
+                _verifyList = new List<string>();
+                return;
+            }
+            //MessageBox.Show(_verifyList);
+            var profileUrlName2 = textBox1.Text.Contains("?id=") ? Regex.Match(textBox1.Text, @"(?<=\?id=).*?(?=(\&|\?))").Value : Regex.Match(textBox1.Text, @"(?<=\.com\/).*?(?=(\&|\?))").Value;
+            if (profileUrlName2 == "")
+            {
+                profileUrlName2 = Regex.Match(textBox1.Text, @"(?<=\.com\/).*", RegexOptions.IgnoreCase).Value;
+            }
+
+            bool found = false;
+        
+            foreach (var s in _verifyList)
+            {
+
+                var profileUrl = Regex.Match(s, @"(?<=href="").*?(?="")").Value;
+                var profileUrlName = profileUrl.Contains("?id=") ? Regex.Match(profileUrl, @"(?<=\?id=).*?(?=(\&|\?))").Value : Regex.Match(profileUrl, @"(?<=\.com\/).*?(?=(\&|\?))").Value;
+
+                if (isUrl)
+                {
+                    if (profileUrlName == profileUrlName2 && profileUrlName != "")
+                    {
+                        //MessageBox.Show(profileUrlName);
+                        //MessageBox.Show(profileUrlName2);
+                        found = true;
+                        _profileMessageUrl = "https://www.facebook.com/messages/t/" + profileUrlName;
+                        labelReport.Text = StripHtml(s);
+                        labelReport.ForeColor = s.Contains("ปลด") ? Color.Red : Color.Green;
+                        buttonFacebookMessageCheck.Enabled = true;
+                        break;
+                    }
+                    
+                }
+                if (upperId != Regex.Match(s, @"PP.*?(?=\-)").Value) continue;
+                // MessageBox.Show(profileUrl);
+                // MessageBox.Show(profileUrlName);
+                found = true;
+                _profileMessageUrl = "https://www.facebook.com/messages/t/" + profileUrlName;
+                labelReport.Text = StripHtml(s);
+                labelReport.ForeColor = s.Contains("ปลด") ? Color.Red : Color.Green;
+                buttonFacebookMessageCheck.Enabled = true;
+                break;
+            }
+            if (!found)
+            {
+                labelReport.Text = @"ไม่พบ.";
+                labelReport.ForeColor = Color.Black;
+            }
+            buttonCheck.Enabled = true;
+        }
+        public static string StripHtml(string input)
+        {
+            return Regex.Replace(input, "<.*?>", String.Empty);
+        }
+        public void GetVerifyList()
+        {
+            string sourceUrl = "https://drive.google.com/uc?export=download&id=0Byc1YSB3Sd7xTUxRS1YyYmRmOFU";
+            var downloadStr = "";
+            for (int i = 0; i < 5; i++)
+            {
+                using (var webpage = new WebClient())
+                {
+                    webpage.Headers[HttpRequestHeader.UserAgent] = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.121 Safari/535.2";
+                    downloadStr = webpage.DownloadString(sourceUrl);
+                }
+                //MessageBox.Show(_verifyList.Length.ToString());
+                if (downloadStr.Length > 1000)
+                {
+                    break;
+                }
+            }
+            //MessageBox.Show(downloadStr);
+            _verifyList = downloadStr.Replace("<br>", Environment.NewLine).Split('\n').ToList();
+            //MessageBox.Show(string.Join("\r\n", _verifyList));
+            if (_verifyList.Count > 100)
+            {
+                labelVerifyListUpdate.Invoke(new MethodInvoker(delegate
+                {
+                    labelVerifyListUpdate.Text = @"รายการ Verify อัพเดท " + _verifyList[0];
+                    labelVerifyListUpdate.ForeColor = Color.DarkGreen;
+                }));
+            }
+        }
+        private string _profileMessageUrl;
+        private void buttonFacebookMessageCheck_Click(object sender, EventArgs e)
+        {
+            Process.Start(_profileMessageUrl);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Version version = Assembly.GetEntryAssembly().GetName().Version;
+            Text += " v" + version;
+            var thread = new Thread(delegate()
+            {
+                try
+                {
+                    GetVerifyList();
+                }
+                catch (Exception ex)
+                {
+
+                    labelReport.Invoke(new MethodInvoker(delegate
+                    {
+                        labelReport.ForeColor = Color.Orange;
+                        labelReport.Text = ex.Message;
+                    }));
+
+                    buttonCheck.Invoke(new MethodInvoker(delegate
+                    {
+                        buttonCheck.Enabled = true;
+                    }));
+                    _verifyList = new List<string>();
+                }
+            });
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+            //thread.Join();
+        }
+
+        private void verifyListOpen_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://www.facebook.com/groups/219354214894010/permalink/298543423641755/");
+        }
+    }
+}
